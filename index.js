db30d2f86b7bd7ada251ce5982189d8c52410e56
Solomiia Kushniryk <kushniryksolomiya@gmail.com>
2023-02-15 12:55:17 +0200

    Start hw data-*

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 "use strict";
 
-const handlerBtn = ({ target: { parentElement: parent , dataset:{bgColor}} }) => {
-    // console.log(target.innerText)
-    parent.style.backgroundColor = bgColor;
+const handlerBtn = ({
+    target: {
+        parentElement: parent,
+        dataset: { direction },
+    },
+}) => {
+    console.log(direction);
+    const boxBtnDir = parent.nextElementSibling;
+    boxBtnDir.style.flexDirection = direction;
 };
 
-const btns = document.querySelectorAll("#wrapper>button");
+const btns = document.querySelectorAll("#data-btn-directon>button");
 
 for (const btn of btns) {
     btn.addEventListener("click", handlerBtn);
