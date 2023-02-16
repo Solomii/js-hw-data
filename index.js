1cdaddc4976948430270bf9bf451701987b1f6de
Solomiia Kushniryk <kushniryksolomiya@gmail.com>
2023-02-16 13:57:41 +0200

    work in progress

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 const handlerBtn = ({
     target: {
         parentElement: parent,
-        dataset: { direction },
+        dataset: { direction},
     },
 }) => {
     console.log(direction);
@@ -11,8 +11,13 @@ const handlerBtn = ({
     boxBtnDir.style.flexDirection = direction;
 };
 
-const btns = document.querySelectorAll("#data-btn-directon>button");
+const btns = document.querySelectorAll("#data-btn-direction>button");
+const btnsDirTest = document.getElementById("box-btn-direction");
+
+btnsDirTest.style.display="flex";
 
 for (const btn of btns) {
     btn.addEventListener("click", handlerBtn);
+    btn.style.padding = "20px";
+    btn.style.border = "1px solid #50c4f1";
 }
