07c8b6c2c991ebf4b0dc9a12849726d9faae05c0
Solomiia Kushniryk <kushniryksolomiya@gmail.com>
2023-02-16 19:27:50 +0200

    Refactoring

--- index.js
+++ index.js
@@ -3,21 +3,23 @@
 const handlerBtn = ({
     target: {
         parentElement: parent,
-        dataset: { direction},
+        dataset: { direction },
     },
 }) => {
-    console.log(direction);
     const boxBtnDir = parent.nextElementSibling;
     boxBtnDir.style.flexDirection = direction;
 };
 
-const btns = document.querySelectorAll("#data-btn-direction>button");
-const btnsDirTest = document.getElementById("box-btn-direction");
+const btnsDataAttribute = document.querySelectorAll(
+    "#box-data-attribute>button"
+);
+const boxDirection = document.getElementById("box-btns-direction");
 
-btnsDirTest.style.display="flex";
+boxDirection.style.display = "flex";
 
-for (const btn of btns) {
+for (const btn of btnsDataAttribute) {
     btn.addEventListener("click", handlerBtn);
     btn.style.padding = "20px";
+    btn.style.margin = "5px";
     btn.style.border = "1px solid #50c4f1";
 }
